d3df21f20000bcbc4cb4096a45e4e3af0f4f8087
Andr3w-Pham <andrew.pham2492@gmail.com>
2018-06-13 13:53:27 +1000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,10 @@ Rails.application.routes.draw do
     member do
       put 'like', to:"blogs#vote"
     end
-    resources :comments , only: [:create]
+
   end
   # devise_for :users
     devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    get 'home/page'
+    
 end
