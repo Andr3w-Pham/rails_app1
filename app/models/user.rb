e687e9868a382ded9a676d7ecea4a645edb0838c
Andr3w-Pham <andrew.pham2492@gmail.com>
2018-06-28 19:04:32 +1000

    email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :email, presence: true
+  validates :email, uniqueness: true
   has_many :followings, dependent: :destroy
   has_many :followees, through: :followings, dependent: :destroy
 
